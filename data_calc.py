a6844f6d65af4c4b5bc6db693bdf1b06e949a087
liudoudou86 <543972930@qq.com>
2022-01-21 12:13:24 +0800

    update coding

--- data_calc.py
+++ data_calc.py
@@ -2,11 +2,25 @@
 
 import pandas as pd
 
-data = pd.DataFrame(pd.read_excel('before.xlsx'))
-d = (data.set_index(['No.','平台']) # 以哪两列为索引
-         .stack() # 将除索引列的其他列堆成行
-         .str.split(' ',expand=True) # 以空格为切割为多个单元格
-         .stack() # 再次堆行
-         .str.split('*',expand=True) # 以*号为切割为多个单元格
-         .unstack(-2)) # 索引倒数第二列成为新的列
-d.to_excel('after.xlsx') # 输出到新的文档
+
+def data():
+	data = pd.DataFrame(pd.read_excel('before.xlsx'))
+	data_order = (data.set_index(['NO','日期','平台','单号','状态','解决方案','数量','快递公司','订单编号'])
+	            .stack()
+				.str.split(' ', expand=True)
+				.stack()
+				.reset_index(drop=False)
+				)
+	data_order.to_excel('after_1st.xlsx') # 需要删除多余列
+	
+def info():
+	info = pd.DataFrame(pd.read_excel('after_1st.xlsx'))
+	info_order = (info.set_index(['NO','日期','平台','单号','状态','解决方案','数量','快递公司','订单编号'])
+	            .stack()
+				.str.split('*', expand=True)
+				.reset_index(drop=False)
+				)
+	info_order.to_excel('after_2nd.xlsx') # 需要删除多余列
+	
+if __name__ == '__main__':
+	info()
